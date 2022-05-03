2beff3fe966ab18ada34c611cd2f07d18e9a8162
CBailey92 <101754989+CBailey92@users.noreply.github.com>
2022-05-02 21:57:52 -0400

    1

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU Arch
+console.log(os.arch());
+
+// CPU core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
